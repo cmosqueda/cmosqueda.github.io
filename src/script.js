8a491a3856ba87a43773170e74341e8294e41509
Christine Mosqueda <mosqueda.christinereisa04@gmail.com>
2024-04-04 21:22:46 +0800

    updated, added about section

--- src/script.js
+++ src/script.js
@@ -11,3 +11,27 @@ function toggleMenu() {
     navSideBar.style.display = "";
   }
 }
+
+let introContinue = document.getElementById("moreContent");
+let readMoreSpan = document.getElementById("readMore");
+let readLessSpan = document.getElementById("readLess");
+let seeMoreToggled = false;
+
+readLessSpan.style.textDecoration = "underline";
+
+// for toggling see more button on about me
+function seeMore() {
+  if (seeMoreToggled == false) {
+    seeMoreToggled = true;
+    introContinue.style.display = "contents";
+    readMoreSpan.style.display = "none";
+    readLessSpan.style.display = "contents";
+
+    readLessSpan.style.textDecoration = "underline";
+  } else {
+    seeMoreToggled = false;
+    introContinue.style.display = "";
+    readMoreSpan.style.display = "contents";
+    readLessSpan.style.display = "";
+  }
+}
