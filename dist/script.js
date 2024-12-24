a6d4a3be1fa7103ce17a98006051675f542fcdaa
potyne <tinesarkiv@gmail.com>
2024-12-24 10:10:32 +0800

    modified header rounded

--- dist/script.js
+++ dist/script.js
@@ -2,9 +2,9 @@ window.addEventListener("scroll", () => {
   const header = document.getElementById("header");
   if (window.scrollY > 50) {
     header.classList.add("shadow-md");
-    header.classList.remove("m-5");
+    header.classList.remove("m-5", "rounded-3xl");
   } else {
-    header.classList.add("m-5");
+    header.classList.add("m-5", "rounded-3xl");
     header.classList.remove("shadow-md");
   }
 });
